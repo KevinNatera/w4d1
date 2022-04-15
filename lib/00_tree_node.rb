1b4d234ff60f9dc3cdf941640332badbe7cfdf59
Kevin <NateraKevin1@gmail.com>
2022-04-15 14:18:55 -0400

    completed phase 2

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,30 @@
+require 'byebug'
+
 class PolyTreeNode
+    attr_reader :value, :parent, :children 
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+
+    def parent=(parent_node)
+        # debugger
+        if @parent != nil 
+        old_parent = self.parent 
+        old_parent.children.delete(self)
+        end 
+
+
+        @parent = parent_node 
+        if parent_node != nil 
+        @parent.children << self 
+        end
+    end
+
+
+
 
 end
